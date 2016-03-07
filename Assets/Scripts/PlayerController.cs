7f8f3a6e2eb777e8ea80c2169187477774aafc93
Jack <catinacan@gmail.com>
2016-03-06 17:52:32 -1000

    Successful test of rollaball with Kinect. Need live data to imrove.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,161 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using Kinect = Windows.Kinect;
+
+public class PlayerController : MonoBehaviour {
+
+   public float speed;
+
+   private Rigidbody rb;
+   private int count;
+   public Text countText;
+   public Text wintText;
+
+
+   public GameObject BodySourceManager;
+
+   private Dictionary<ulong, GameObject> _Bodies = new Dictionary<ulong, GameObject>();
+   private BodySourceManager _BodyManager;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        wintText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        // float moveVertical = Input.GetAxis("Vertical");
+
+        // Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        Vector3 movement = Vector3.zero;
+
+             if (BodySourceManager == null)
+        {
+            return;
+        }
+        
+        _BodyManager = BodySourceManager.GetComponent<BodySourceManager>();
+        if (_BodyManager == null)
+        {
+            return;
+        }
+        
+        Kinect.Body[] data = _BodyManager.GetData();
+        if (data == null)
+        {
+            return;
+        }
+
+        List<ulong> trackedIds = new List<ulong>();
+        foreach (var body in data) {
+            if (body == null) {
+                continue;
+            }
+
+            if (body.IsTracked) {
+                trackedIds.Add(body.TrackingId);
+            }
+        }
+
+        List<ulong> knownIds = new List<ulong>(_Bodies.Keys);
+
+        // First delete untracked bodies
+        foreach (ulong trackingId in knownIds) {
+            if (!trackedIds.Contains(trackingId)) {
+                Destroy(_Bodies[trackingId]);
+                _Bodies.Remove(trackingId);
+            }
+        }
+        foreach (Kinect.Body body in data) {
+
+            if (body.IsTracked) {
+
+
+                Kinect.Joint rightHand = body.Joints[Kinect.JointType.HandRight];
+                Kinect.Joint leftHand = body.Joints[Kinect.JointType.HandLeft];
+
+                Kinect.Joint spineMid = body.Joints[Kinect.JointType.SpineMid];
+                Kinect.Joint spineBase = body.Joints[Kinect.JointType.SpineBase];
+
+                if (rightHand.Position.X > spineMid.Position.X) {
+                    movement = Vector3.right;
+                }
+                else if (rightHand.Position.X < spineMid.Position.X) {
+                    movement = Vector3.left;
+                }
+                else {
+                    movement = Vector3.zero;
+                }
+            }
+        }
+
+        rb.AddForce(movement * speed);
+	}
+
+    void FixedUpdate()
+    {
+      
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 6)
+        {
+            wintText.text = "You win!";
+        }
+    }
+
+
+    private Dictionary<Kinect.JointType, Kinect.JointType> _BoneMap = new Dictionary<Kinect.JointType, Kinect.JointType>()
+    {
+        { Kinect.JointType.FootLeft, Kinect.JointType.AnkleLeft },
+        { Kinect.JointType.AnkleLeft, Kinect.JointType.KneeLeft },
+        { Kinect.JointType.KneeLeft, Kinect.JointType.HipLeft },
+        { Kinect.JointType.HipLeft, Kinect.JointType.SpineBase },
+        
+        { Kinect.JointType.FootRight, Kinect.JointType.AnkleRight },
+        { Kinect.JointType.AnkleRight, Kinect.JointType.KneeRight },
+        { Kinect.JointType.KneeRight, Kinect.JointType.HipRight },
+        { Kinect.JointType.HipRight, Kinect.JointType.SpineBase },
+        
+        { Kinect.JointType.HandTipLeft, Kinect.JointType.HandLeft },
+        { Kinect.JointType.ThumbLeft, Kinect.JointType.HandLeft },
+        { Kinect.JointType.HandLeft, Kinect.JointType.WristLeft },
+        { Kinect.JointType.WristLeft, Kinect.JointType.ElbowLeft },
+        { Kinect.JointType.ElbowLeft, Kinect.JointType.ShoulderLeft },
+        { Kinect.JointType.ShoulderLeft, Kinect.JointType.SpineShoulder },
+        
+        { Kinect.JointType.HandTipRight, Kinect.JointType.HandRight },
+        { Kinect.JointType.ThumbRight, Kinect.JointType.HandRight },
+        { Kinect.JointType.HandRight, Kinect.JointType.WristRight },
+        { Kinect.JointType.WristRight, Kinect.JointType.ElbowRight },
+        { Kinect.JointType.ElbowRight, Kinect.JointType.ShoulderRight },
+        { Kinect.JointType.ShoulderRight, Kinect.JointType.SpineShoulder },
+        
+        { Kinect.JointType.SpineBase, Kinect.JointType.SpineMid },
+        { Kinect.JointType.SpineMid, Kinect.JointType.SpineShoulder },
+        { Kinect.JointType.SpineShoulder, Kinect.JointType.Neck },
+        { Kinect.JointType.Neck, Kinect.JointType.Head },
+    };
+
+   
+
+}
